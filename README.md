54c22c1ca4a6a738ad2fd0bbc06a39d1ee6e10f0
Michal Hloch <hloch.michal@gmail.com>
2026-09-12 12:45:20 +0200

    removed files

--- README.md
+++
@@ -1,2 +0,0 @@
-# road-something-simulator
-Attempt at creating a road planner/driver simulator in Godot C#.
--- src/Traffic.EngineClient/Utils/VectorExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace RoadTrafficSim.Utils;
-
-using System.Runtime.CompilerServices;
-
-public static class VectorExtensions
-{
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Godot.Vector3 ToGodot(this System.Numerics.Vector3 vector) => new(vector.X, vector.Y, vector.Z);
-}
